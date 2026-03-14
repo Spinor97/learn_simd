874eb09796f2a0e4bc2116bf29d7c1472055bbf4
Spinor <kaihuiluo2@gmail.com>
2026-03-14 23:31:17 +0800

    add gitignore, enhance files

--- include/CSVTable.h
+++ include/CSVTable.h
@@ -2,32 +2,37 @@
 
 #include <vector>
 #include <string>
+#include "col_type.h"
+#include <memory>
 
 struct CSVTable {
     std::string buffer;
-    std::vector<size_t> row;
-    std::vector<size_t> col_stt;
-    std::vector<size_t> col_end;
+    // std::vector<size_t> row;
+    // std::vector<size_t> col_stt;
+    // std::vector<size_t> col_end;
+
+    std::vector<std::unique_ptr<ColType::baseType>> columns;
+    size_t row_cnt = 0;
 
     size_t getRow() const {
-        return row.size();
+        return row_cnt;
     }
 
     size_t getCol(size_t r) const {
-        size_t stt = row[r];
-        size_t ed = (r + 1 < getRow())
-            ? row[r + 1]
-            : col_stt.size();
+        // size_t stt = row[r];
+        // size_t ed = (r + 1 < getRow())
+        //     ? row[r + 1]
+        //     : col_stt.size();
 
-        return ed - stt;
+        return columns.size();
     }
 
-    std::string_view get(size_t r, size_t c) noexcept {
-        size_t idx = row[r] + c; 
+    // std::string_view get(size_t r, size_t c) noexcept {
+    //     size_t idx = row[r] + c; 
 
-        return std::string_view(
-            buffer.data() + col_stt[idx],
-            col_end[idx] - col_stt[idx]
-        );
-    }
+    //     return std::string_view(
+    //         buffer.data() + col_stt[idx],
+    //         col_end[idx] - col_stt[idx]
+    //     );
+    // }
 };
--- include/csv_builder.h
+++ include/csv_builder.h
@@ -2,13 +2,19 @@
 
 #include "csv_parser.h"
 #include "CSVTable.h"
+#include "col_type.h"
+#include <memory>
+#include <stdexcept>
+
+using colType = ColType::Tp;
 
 class CSVBuilder {
 public:
-    explicit CSVBuilder(std::string data)
-        : table{}
+    explicit CSVBuilder(std::string data, std::vector<colType> types)
+        : table{}, schema_(std::move(types))
     {
         table.buffer = std::move(data);
+        init_col();
     }
 
     CSVTable build() noexcept {
@@ -18,19 +24,57 @@ public:
 
 private:
     CSVTable table;
+    std::vector<colType> schema_;
 
-    void parse() noexcept {
+    void init_col() {
+        size_t expected_rows = table.buffer.length() / 32;
+        table.columns.reserve(schema_.size());
+
+        for (auto type : schema_) {
+            switch (type) {
+                case colType::Int64: {
+                    auto col =
+                        std::make_unique<ColType::intType>();
+                    col->data.reserve(expected_rows);
+                    table.columns.push_back(std::move(col));
+                    break;
+                }
+
+                case colType::Double: {
+                    auto col =
+                        std::make_unique<ColType::doubleType>();
+                    col->data.reserve(expected_rows);
+                    table.columns.push_back(std::move(col));
+                    break;
+                }
+
+                case colType::String: {
+                    auto col =
+                        std::make_unique<ColType::stringType>();
+                    col->data.reserve(expected_rows);
+                    table.columns.push_back(std::move(col));
+                    break;
+                }
+
+                default:
+                    throw std::runtime_error("Unknown col");
+            }
+        }
+    }
+
+    void parse() {
         const char* ptr = table.buffer.data();
         size_t len = table.buffer.size();
 
         uint32_t quote_carry = 0;
         size_t field_start = 0;
-
-        table.row.push_back(0);
+        size_t current_column = 0;
 
         size_t i = 0;
+
         while (i + 32 <= len) {
             auto masks = scan(ptr + i);
+
             uint32_t in_string =
                 prexor(masks.quote_msk, quote_carry);
 
@@ -41,7 +85,8 @@ private:
                 masks.nl_msk & ~in_string;
 
             process_mask(commas, newlines,
-                         i, field_start);
+                        i, field_start,
+                        current_column);
 
             i += 32;
         }
@@ -51,50 +96,63 @@ private:
             char c = ptr[i];
 
             if (c == ',') {
-                add_field(field_start, i);
+                add_field(field_start, i, current_column);
                 field_start = i + 1;
             }
             else if (c == '\n') {
-                add_field(field_start, i);
-                table.row.push_back(
-                    table.col_stt.size());
+                add_field(field_start, i, current_column);
+
+                if (current_column != table.columns.size())
+                    throw std::runtime_error("Column mismatch");
+
+                current_column = 0;
+                ++table.row_cnt;
+
                 field_start = i + 1;
             }
         }
 
         if (field_start < len) {
-            add_field(field_start, len);
+            add_field(field_start, len, current_column);
         }
     }
 
-    inline void process_mask(uint32_t commas,
-                             uint32_t newlines,
-                             size_t base,
-                             size_t& field_start) {
-
+    inline void process_mask(uint32_t commas, uint32_t newlines, size_t base,
+                         size_t& field_start,
+                         size_t& current_column) {
         while (commas) {
             int bit = __builtin_ctz(commas);
             size_t pos = base + bit;
-            add_field(field_start, pos);
+
+            add_field(field_start, pos, current_column);
             field_start = pos + 1;
+
             commas &= commas - 1;
         }
 
         while (newlines) {
             int bit = __builtin_ctz(newlines);
             size_t pos = base + bit;
-            add_field(field_start, pos);
-            table.row.push_back(
-                table.col_stt.size());
+
+            add_field(field_start, pos, current_column);
+
+            if (current_column != table.columns.size())
+                throw std::runtime_error("Column mismatch");
+
+            current_column = 0;
+            ++table.row_cnt;
+
             field_start = pos + 1;
             newlines &= newlines - 1;
         }
     }
 
-    inline void add_field(size_t start, size_t end) {
-        table.col_stt.push_back(start);
-        table.col_end.push_back(end);
-    }
+    inline void add_field(size_t start, size_t end, size_t& current_column) {
+        const char* ptr = table.buffer.data() + start;
+        size_t len = end - start;
 
-    CSVTable table;
+        table.columns[current_column]->add(ptr, len);
+
+        ++current_column;
+    }
 };
